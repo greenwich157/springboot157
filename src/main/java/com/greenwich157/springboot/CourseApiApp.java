acec10e47f63a94878325b04fdd5d2817faae528
greenwich157 <greenwich157@gmail.com>
2018-04-12 20:39:58 +0800

    Update CourseApiApp.java

--- src/main/java/com/greenwich157/springboot/CourseApiApp.java
+++ src/main/java/com/greenwich157/springboot/CourseApiApp.java
@@ -1,14 +1,18 @@
 package com.greenwich157.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApiApp {
+public class CourseApiApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(CourseApiApp.class, args);
-
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MicroserviceBootApplication.class);
 
 }
