3677ac4c1a0f119be27d10c854b41ec673ae2403
Louis Lai <greenwich157@gmail.com>
2018-04-13 22:45:20 +0800

    vvv

---
+++ src/main/java/com/greenwich157/springboot/hello/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.greenwich157.springboot.hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+	@Test
+	void testSayHi() {
+		fail("Not yet implemented");
+	}
+
+}
